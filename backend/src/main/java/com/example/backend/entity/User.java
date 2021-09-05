06e537b53d640848121eb0760da8ab6ccbbba6fc
singhvisha <vishal16.singh@paytm.com>
2021-09-05 20:52:14 +0530

    Modified the PollEntity and UserEntity

--- backend/src/main/java/com/example/backend/entity/User.java
+++ backend/src/main/java/com/example/backend/entity/User.java
@@ -2,13 +2,7 @@ package com.example.backend.entity;
 
 import java.util.List;
 
-import javax.persistence.Column;
-import javax.persistence.Entity;
-import javax.persistence.GeneratedValue;
-import javax.persistence.GenerationType;
-import javax.persistence.Id;
-import javax.persistence.OneToMany;
-import javax.persistence.Table;
+import javax.persistence.*;
 
 import lombok.Data;
 import lombok.NoArgsConstructor;
@@ -39,6 +33,6 @@ public class User {
 	@Column(name="interest", nullable=true)
 	private String interest;
 	
-	@OneToMany(mappedBy="user")
+	@OneToMany(mappedBy="user", cascade = CascadeType.ALL)
 	List<PollEntity> polls; 
 }
